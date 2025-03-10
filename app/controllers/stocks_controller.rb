de773040d72c6484655eca53fdd2b95c74022b04
LebedenkoAndrii <lebedenko1928@gmail.com>
2025-03-11 01:04:32 +0200

    display data on page with fake data, because have api limit

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,8 @@
 class StocksController < ApplicationController
   def search
-    stock = Stock.new_lookup(params[:stock])
-    render json: stock
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+    end
+    render "users/my_portfolio"
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,10 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
+=begin
     crypto = Alphavantage::Crypto.new(symbol: ticker_symbol, market: 'USD')
     
     daily_data = crypto.daily
+    puts "Daily data: #{daily_data.inspect}"
     
     unless daily_data&.time_series_digital_currency_daily
       raise "Не вдалося отримати дані для #{ticker_symbol}. Перевірте API-ключ або символ."
@@ -11,10 +13,11 @@ class Stock < ApplicationRecord
     last_day = daily_data.time_series_digital_currency_daily.first
     
     price = last_day[1]['close'].to_f
-    
-    price
-  rescue StandardError => e
-    puts "Помилка в new_lookup: #{e.message}"
-    nil
+    name = daily_data.meta_data.digital_currency_name
+=end
+    { ticker: ticker_symbol, name: "Bitcoin", price: 80000 }
+  #rescue StandardError => e
+    #puts "Помилка в new_lookup: #{e.message}"
+    #nil
   end
 end
