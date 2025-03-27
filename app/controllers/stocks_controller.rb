58027280ce6ffbee6a1c4e2c5f2fcc3036218f9b
LebedenkoAndrii <lebedenko1928@gmail.com>
2025-03-28 01:46:27 +0200

    all is working, but need rerender

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,39 +1,18 @@
 class StocksController < ApplicationController
+ 
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
-      respond_to do |format|
-        if @stock
-          format.turbo_stream
-          format.html { render 'users/my_portfolio' }
-        else
-          flash.now[:alert] = "Please enter a valid symbol to search"
-          format.turbo_stream do
-            render turbo_stream: [
-              turbo_stream.replace("messages", partial: "layouts/messages"),
-              turbo_stream.update("results", "")
-            ]
-          end          
-          format.html do
-            flash[:alert] = "Please enter a valid symbol to search"
-            redirect_to my_portfolio_path
-          end
-        end
-      end
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:alert] = "Please enter a valid symbol to search"
+        redirect_to my_portfolio_path
+      end    
     else
-      flash.now[:alert] = "Please enter a symbol to search"
-      respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: [
-            turbo_stream.replace("messages", partial: "layouts/messages"),
-            turbo_stream.update("results", "")
-          ]
-        end        
-        format.html do
-          flash[:alert] = "Please enter a symbol to search"
-          redirect_to my_portfolio_path
-        end
-      end
+      flash[:alert] = "Please enter a symbol to search"
+      redirect_to my_portfolio_path
     end
   end
+
 end
